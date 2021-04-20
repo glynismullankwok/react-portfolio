5676e7729b0493e92303341c6a2d7c17792e1499
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2021-04-20 18:01:14 -0400

    react file system and combine html

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import Home from './pages/home';
+import Portfolio from './pages/portfolio';
+import Contact from './pages/contact';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return <Home />
+  return <Portfolio />
+  return <Contact />
+
+}
+
+export default App;
